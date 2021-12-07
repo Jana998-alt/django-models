f8cff2df8bdb1dd1f53e67c1305a7e248fc6fa11
Jana Abdelkader <janaosama1300@gmail.com>
2021-12-07 16:17:21 +0200

    fixed the model

--- snacks/models.py
+++ snacks/models.py
@@ -2,9 +2,14 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 from django.contrib.auth import get_user_model
 
+
 # Create your models here.
 
 class Snack(models.Model):
     name = models.TextField(max_length=64)
-    purchaser = models.ForeignKey(to = get_user_model(),on_delete=(CASCADE), default= 1)
-    description = models.TextField()
+    purchaser = models.ForeignKey(to = get_user_model(),on_delete=(models.CASCADE), default= 1)
+    description = models.TextField(max_length=256)
+
+    def __str__(self) -> str:
+        return self.name
+        
--- snacks/views.py
+++ snacks/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
+from snacks.models import Snack
+
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+    model = Snack
